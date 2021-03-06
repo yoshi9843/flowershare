7365255fc5f46b6b980047767b7c0189dd2f75ae
yoshi353 <jiajidaye753@gmail.com>
2021-03-06 02:45:17 +0000

    Improve routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root 'users#index'
+  resources :users
+  resources :flowers
+  resources :comments
+  resources :likes, only: [:create, :destroy]
 end
+
+
