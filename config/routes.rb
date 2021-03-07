535006ad84569340a8936292eb92401471863911
yoshi353 <jiajidaye753@gmail.com>
2021-03-07 05:38:50 +0000

    Make user cotroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root 'users#index'
-  resources :users
-  resources :flowers
-  resources :comments
-  resources :likes, only: [:create, :destroy]
+  
 end
 
 
