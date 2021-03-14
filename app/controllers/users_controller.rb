d73863856583c8ed6f0750a873440b558141c1c2
yoshi353 <jiajidaye753@gmail.com>
2021-03-14 04:50:22 +0000

    Make users html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,11 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+  
+  def edit
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
   get 'pages/show'
+  resources :users
+  resources :flowers
+  resources :comments
+  resources :likes, only: [:create, :destroy]
 end
 
 
